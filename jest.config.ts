f5c89cc216ee31c54da2f121a7ba12f05fe4b419
penkov <penkov.vladimir@gmail.com>
2021-03-07 12:05:06 +0300

    fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
-module.exports = {
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '.',
   roots: ['test'],
   collectCoverageFrom: ["src/**/*.ts"]
 };
+export default config;
