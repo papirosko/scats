38af2f3b449b8dea2af60179a58cd2698a6c574a
Igor Agafonov <igoradm90@gmail.com>
2024-11-26 16:40:59 +0100

    fix collection concat

--- src/collection.ts
+++ src/collection.ts
@@ -57,7 +57,7 @@ export abstract class ArrayBackedCollection<T, C extends ArrayIterable<T, any>> 
     }
 
     appendedAll(other: Iterable<T>): C {
-        return this.fromArray(this.items.concat(...other));
+        return this.fromArray(this.items.concat([...other]));
     }
 
     prepended(item: T): C {
@@ -69,7 +69,7 @@ export abstract class ArrayBackedCollection<T, C extends ArrayIterable<T, any>> 
     }
 
     concat(other: Iterable<T>): C {
-        return this.appendedAll(other);
+        return this.fromArray(this.items.concat([...other]));
     }
 
     slice(from: number, until: number): C {
